d80241179114556572e55923fabdd2c19d42c808
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-09-06 12:58:36 +0700

    refactor: update environment variables and expand whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 import { env } from '~/config/environment'
 // Domains that are allowed to access the API
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173',
+  'https://todolist-web-fe.vercel.app'
+]
 
 export const WEBSITE_DOMAIN =
   env.BUILD_MODE === 'production'
