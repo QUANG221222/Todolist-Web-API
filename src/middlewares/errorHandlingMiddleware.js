aa18284e61337324bfb6006fae1d17beb3bd1f36
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-09-06 11:58:50 +0700

    refactor: reorder error handling logic for clarity

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -6,15 +6,15 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   // If the developer forgets to set statusCode, default to 500 INTERNAL_SERVER_ERROR
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
 
-  // If Build Mode is not 'dev', remove the stack trace from the response
-  if (env.BUILD_MODE !== 'dev') delete responseError.stack
-
   // Create a variable responseError to control the response
   const responseError = {
     statusCode: err.statusCode,
     message: err.message || StatusCodes[err.statusCode],
     stack: err.stack
   }
+
+  // If Build Mode is not 'dev', remove the stack trace from the response
+  if (env.BUILD_MODE !== 'dev') delete responseError.stack
 
   res.status(responseError.statusCode).json(responseError)
 }
